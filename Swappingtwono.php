8bee919c039137ad03e2c2e40c8892c1e8c04137
poojayadav1999 <103725966+poojayadav1999@users.noreply.github.com>
2022-07-31 12:49:52 +0530

    Upload PHP Program

---
+++ Swappingtwono.php
@@ -0,0 +1,20 @@
+//Program For Swapping Two Numbers
+
+<?php
+   $a=10;
+   $b=20;
+   echo "<br>";
+   echo "Before Swapping <br>";
+   echo "a = $a";
+   echo "<br>";
+   echo "b = $b";
+   $a=$a+$b;
+   $b=$a-$b;
+   $a=$a-$b;
+   echo "<br>";
+   
+   echo "After Swapping <br>";
+   echo "a = $a";
+   echo "<br>";
+   echo "b = $b" ;
+?>
